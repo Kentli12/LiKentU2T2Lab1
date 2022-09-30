90b9a23aa552a7b9abc580e58fdd6654df060842
vanessa <vanessah43@nycstudents.net>
2022-09-30 12:33:29 -0400

    School work

---
+++ src/Point.java
@@ -0,0 +1,18 @@
+public class Point {
+    //instances variables
+    private int x;
+    private int y;
+
+    //constructor that accepts both values
+    public Point (int x, int, y){
+        this.x = x;
+        this.y = y;
+    }
+    //constructor that accepts one value
+    public Point (int num) {
+        x = num;
+        y = num;
+    }
+
+
+}
--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class RectangleRunner {
+public class RectangleRunner {
+    public static void main (String[] args){
+//    Rectangle plot1 = new Rectangle(150,200);
+//    Rectangle plot2 = new Rectangle(125);
+//    Rectangle plot3 = new Rectangle();
+//
+//    int plot1Area = plot1.calculateArea();
+//    int plot2Area = plot2.calculateArea();
+//    int plot3Area = plot3.calculateArea();
+//
+//    plot1.setWidth(75);
+//    plot2.setWidth(75);
+//    plot3.setWidth(75);
+//    plot2.setLength(75);
+//
+//    System.out.println("Plot 1 length: " + plot1.getLength());
+//    System.out.println("Plot 1 width: " + plot1.getWidth());
+//    System.out.println("Plot 1 area: " + plot1Area);
+//    System.out.println("");
+//    System.out.println("Plot 2 length: " + plot2.getLength());
+//    System.out.println("Plot 2 width: " + plot2.getWidth());
+//    System.out.println("Plot 2 area: " + plot2Area);
+//    System.out.println("");
+//    System.out.println("Plot 3 length: " + plot3.getLength());
+//    System.out.println("Plot 3 width: " + plot3.getWidth());
+//    System.out.println("Plot 3 area: " + plot3Area);
+//    System.out.println("These three plots requires " +(plot1Area + plot2Area + plot3Area)+ " square feet of seed");
+
+//        int len = 65;
+//        int wid = len + 10;
+//        Rectangle rect1 = new Rectangle(len, wid);
+//        Rectangle rect2 = new Rectangle(10.0, 30.0);
+//        Rectangle rect3 = new Rectangle(0);
+
+    }
 }
